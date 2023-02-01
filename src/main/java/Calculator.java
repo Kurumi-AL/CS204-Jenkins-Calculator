f56cf79d86820db7a0a0dbbb7db5197b5bb21cbb
Kurumi Lawrence <rinnrinn292006@gmail.com>
2023-02-01 11:53:56 -0700

    Implement functions and pass all tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.UUID;
 
 class Calculator {
 
@@ -39,7 +40,10 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        return 0;
+        if ((n == 0) || (n == 1))
+            return n;
+        else
+            return fibonacciNumberFinder(n - 1) + fibonacciNumberFinder(n - 2);
     }
 
 
@@ -51,7 +55,7 @@ class Calculator {
     if int a = 16 then this method returns: 10000
      */
     String intToBinaryNumber(int n){
-        return null;
+        return Integer.toBinaryString(n);
     }
 
     /*
@@ -63,8 +67,8 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-
-        return null;
+        String uniqueID = n + UUID.randomUUID().toString();
+        return uniqueID;
     }
 
 
